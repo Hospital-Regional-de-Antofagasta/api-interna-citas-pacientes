8858ce448ebecd130315a92d475fd8ae4fe8af8e
Claudio González Salazar <pyrates_claudio@hotmail.com>
2021-07-02 09:46:04 -0400

    HRAP-148: CHANGE: Se cambia forach por for en controlador de solicitudes Anular/Cambiar citas pacientes.

--- api/controllers/solicitudesCitasPacientesController.js
+++ api/controllers/solicitudesCitasPacientesController.js
@@ -9,13 +9,13 @@ exports.getNuevasSolicitudesAnularCambiarCitasPacientes = async (req, res) => {
         .sort({ createdAt: 1 })
         .limit(100)
         .exec();
-    solicitudesAnularCambiarCitasPacientes.forEach(async (solicitud) => {
+    for (const solicitud of solicitudesAnularCambiarCitasPacientes) {
       solicitud.enviadaHospital = true;
       await SolicitudesAnularCambiarCitasPacientes.updateOne(
         { _id: solicitud._id },
         { enviadaHospital: true }
       ).exec();
-    });
+    }
     res.status(200).send(solicitudesAnularCambiarCitasPacientes);
   } catch (error) {
     res.status(500).send({
