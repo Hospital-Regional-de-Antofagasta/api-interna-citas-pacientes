df1f4866f4bed8795ccac10386ae584615daaba3
Claudio González Salazar <pyrates_claudio@hotmail.com>
2021-08-31 14:00:00 -0400

    HRAP-225: ADD: Cambios faltantes.

--- api/app.js
+++ api/app.js
@@ -1,16 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-
+require("dotenv").config();
 const citasPacientes = require("./routes/citasPacientes");
 const solicitudesCitasPacientes = require("./routes/solicitudesCitasPacientes");
-
 const app = express();
-
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, {
+const connection = process.env.MONGO_URI;
+const port = process.env.PORT;
+const localhost = process.env.HOSTNAME;
+
+mongoose.connect(connection, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
@@ -21,5 +23,15 @@ app.use(
   "/hradb-a-mongodb/citas-pacientes/solicitudes",
   solicitudesCitasPacientes
 );
+
+if (require.main === module) {
+  // true if file is executed
+  process.on("SIGINT", function () {
+    process.exit();
+  });
+  app.listen(port, () => {
+    console.log(`App listening at http://${localhost}:${port}`);
+  });
+}
 
 module.exports = app;
