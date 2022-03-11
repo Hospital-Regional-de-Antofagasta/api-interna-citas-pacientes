2bec6b3b8eca8e6db59e647c303fe9c484384277
Pablo Fontecilla <pafonbu@gmail.com>
2022-03-11 13:35:55 -0300

    FIX: require en modelo a required

--- api/models/CitasPacientes.js
+++ api/models/CitasPacientes.js
@@ -3,9 +3,9 @@ const Schema = mongoose.Schema;
 
 const env = process.env.NODE_ENV;
 
-let db = "hrapp_citas_pacientes"
+let db = "hrapp_citas_pacientes";
 
-if(env==="test") db = `${db}_test`
+if (env === "test") db = `${db}_test`;
 
 const conection = mongoose.connection.useDb(db);
 
@@ -13,22 +13,22 @@ const CitasPacientes = conection.model(
   "citas_paciente",
   new Schema(
     {
-      correlativo: { type: Number, require: true },
-      codigoLugar: { type: String, require: true },
+      correlativo: { type: Number, required: true },
+      codigoLugar: { type: String, required: true },
       nombreLugar: String,
-      codigoServicio: { type: String, require: true },
+      codigoServicio: { type: String, required: true },
       nombreServicio: String,
-      codigoProfesional: { type: String, require: true },
+      codigoProfesional: { type: String, required: true },
       nombreProfesional: String,
-      tipo: { type: String, require: true },
-      codigoAmbito: { type: String, require: true },
-      fechaCitacion: { type: Date, require: true },
-      horaCitacion: { type: String, require: true },
-      rutPaciente: { type: String, require: true },
+      tipo: { type: String, required: true },
+      codigoAmbito: { type: String, required: true },
+      fechaCitacion: { type: Date, required: true },
+      horaCitacion: { type: String, required: true },
+      rutPaciente: { type: String, required: true },
       alta: { type: Boolean, default: false },
       bloqueadaEl: Date,
-      codigoEstablecimiento: { type: String, require: true },
-      nombreEstablecimiento: { type: String, require: true },
+      codigoEstablecimiento: { type: String, required: true },
+      nombreEstablecimiento: { type: String, required: true },
     },
     { timestamps: true }
   )
