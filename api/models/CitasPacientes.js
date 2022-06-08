903f6acd5170d6bd64006cb151098cfad9971a5f
pafonbu <pafonbu@gmail.com>
2022-06-08 09:22:55 -0400

    REMOVE: modelo antiguo

--- api/models/CitasPacientes.js
+++ api/models/CitasPacientes.js
@@ -1,15 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const env = process.env.NODE_ENV;
-
-let db = "hrapp_citas_pacientes";
-
-if (env === "test") db = `${db}_test`;
-
-const conection = mongoose.connection.useDb(db);
-
-const CitasPacientes = conection.model(
+const CitasPacientes = mongoose.model(
   "citas_paciente",
   new Schema(
     {
